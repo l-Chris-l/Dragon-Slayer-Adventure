894c85d2bd244e17a97c56fe454269c411e44ea7
Chirstian Kendall <49040192+l-Chris-l@users.noreply.github.com>
2025-02-16 00:48:25 -0500

    Changed/Added Items in shop

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ class Knight:
         self.attack = 25
         self.defense = 15
         self.magic = 0
+        self.gold = 100
         self.name = "Knight"
     
     def attack_enemy(self):
         return random.randint(self.attack -5, self.attack +5)
     
     def defend (self):
-        return random.randint(self.defense -3, self.defense+3)
+        return random.randint(self.defense -3, self.defense +3)
     
 
 class Archer:
@@ -22,6 +23,7 @@ class Archer:
         self.attack = 35
         self.defense = 10
         self.magic = 0
+        self.gold = 100
         self.name = "Archer"
 
     def attack_enemy(self):
@@ -37,6 +39,7 @@ class Mage:
         self.attack = 15
         self.defense = 5
         self.magic = 40
+        self.gold = 100
         self.name = "Mage"
 
     def attack_enemy(self):
@@ -53,11 +56,16 @@ class Mage:
 class shop:
     def __init__(self):
         self.items = {
-            "Health Potions": 20,
-            "Armor": 50,
-            "Magic Scroll": 70,
-            "Ice Sword": 100,
-            "Fire Sword": 100
+           "Basic Health Potion": 20,
+            "Greater Health Potion": 50,
+            "Supreme Health Potion": 100,
+            "Basic Armor": 50,
+            "Enhanced Armor": 100,
+            "Legendary Armor": 200,
+            "Scroll of Strength": 50,
+            "Scroll of Defense": 50,
+            "Scroll of Magic": 50,
+            "Scroll of Vitality": 75
         }
 
     def show_items(self):
@@ -67,18 +75,53 @@ class shop:
             print(f"{item}: {price} gold")
 
     def buy_items(self, player, item):
-        if item == "Health Potion" and player.gold >= self.items[item]:
+        if item == "Basic Health Potion" and player.gold >= self.items[item]:
             player.gold -= self.items[item]
-            player.health += 50
-            print(f"\nYou bought a Health Potion! Your health is now {player.health}.")
-        elif item =="Armor" and player.gold >= self.items[item]:
+            player.health += 30
+            print(f"\nYou bought a Basic Health Potion! Your health is now {player.health}.")
+        elif item == "Greater Health Potion" and player.gold >= self.items[item]:
+            player.gold -= self.items[item]
+            player.health += 60
+            print(f"\nYou bought a Greater Health Potion! Your health is now {player.health}.")
+        elif item == "Supreme Health Potion" and player.gold >= self.items[item]:
+            player.gold -= self.items[item]
+            player.health += 100
+            print(f"\nYou bought a Supreme Health Potion! Your health is now {player.health}.")
+        elif item == "Basic Armor" and player.gold >= self.items[item]:
             player.gold -= self.items[item]
             player.defense += 10
-            print(f'\nYou bought Armor! Your defense is now {player.defense}')
-        elif item == "Magic Scroll" and player.gold >= self.items[item]:
+            print(f"\nYou bought Basic Armor! Your defense is now {player.defense}.")
+        elif item == "Enhanced Armor" and player.gold >= self.items[item]:
             player.gold -= self.items[item]
-            player.magic += 20
-            print(f'\nYou bought a Magic Scroll! Your magic power is now {player.magic}.')
+            player.defense += 20
+            print(f"\nYou bought Enhanced Armor! Your defense is now {player.defense}.")
+        elif item == "Legendary Armor" and player.gold >= self.items[item]:
+            player.gold -= self.items[item]
+            player.defense += 40
+            print(f"\nYou bought Legendary Armor! Your defense is now {player.defense}.")
+        elif item == "Scroll of Strength" and player.gold >= self.items[item]:
+            player.gold -= self.items[item]
+            player.attack += 10
+            print(f"\nYou bought a Scroll of Strength! Your attack is now {player.attack}.")
+        elif item == "Scroll of Defense" and player.gold >= self.items[item]:
+            player.gold -= self.items[item]
+            player.defense += 10
+            print(f"\nYou bought a Scroll of Defense! Your defense is now {player.defense}.")
+        elif item == "Scroll of Magic" and player.gold >= self.items[item]:
+            player.gold -= self.items[item]
+            player.magic += 10
+            print(f"\nYou bought a Scroll of Magic! Your magic is now {player.magic}.")
+        elif item == "Scroll of Vitality" and player.gold >= self.items[item]:
+            player.gold -= self.items[item]
+            player.health += 50
+            print(f"\nYou bought a Scroll of Vitality! Your health is now {player.health}.")
         else:
             print("\nYou don't have enough gold for that item.")
-   
+
+
+#DRAGON CLASS
+class Dragon:
+    def __init__(self, name, health, attack):
+        self.name = name
+        self.health = health
+        self.attack = attack
