5c1e373e2d2eee2ee168f7473aa73eecc50b27c4
Chirstian Kendall <49040192+l-Chris-l@users.noreply.github.com>
2025-02-16 00:25:43 -0500

    Added Shop System

--- main.py
+++ main.py
@@ -48,4 +48,37 @@ class Mage:
     def magic_attack(self):
         return random.randint(self.magic -10, self.magic +10)
 
-    
+
+ #SHOP SYSTEM
+class shop:
+    def __init__(self):
+        self.items = {
+            "Health Potions": 20,
+            "Armor": 50,
+            "Magic Scroll": 70,
+            "Ice Sword": 100,
+            "Fire Sword": 100
+        }
+
+    def show_items(self):
+        print('\nWelcome to the Shop!')
+        print("Available items:")
+        for item, price in self.items.items():
+            print(f"{item}: {price} gold")
+
+    def buy_items(self, player, item):
+        if item == "Health Potion" and player.gold >= self.items[item]:
+            player.gold -= self.items[item]
+            player.health += 50
+            print(f"\nYou bought a Health Potion! Your health is now {player.health}.")
+        elif item =="Armor" and player.gold >= self.items[item]:
+            player.gold -= self.items[item]
+            player.defense += 10
+            print(f'\nYou bought Armor! Your defense is now {player.defense}')
+        elif item == "Magic Scroll" and player.gold >= self.items[item]:
+            player.gold -= self.items[item]
+            player.magic += 20
+            print(f'\nYou bought a Magic Scroll! Your magic power is now {player.magic}.')
+        else:
+            print("\nYou don't have enough gold for that item.")
+   
